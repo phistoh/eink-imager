ceafca99c2cd68a99b04c333f6c2eb8c60a1f0c7
phistoh <phis@mogobb.de>
2026-05-13 23:12:23 +0200

    changed template location and added `random_view/` endpoint for testing

--- app/web.py
+++ app/web.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 BASE_DIR = Path(__file__).resolve().parents[1]
 app = Flask(
     __name__,
-    template_folder=str(BASE_DIR / "app" / "templates"),
+    template_folder=str(BASE_DIR / "data" / "templates"),
     static_folder=str(BASE_DIR / "static"),
     static_url_path="/static",
 )
@@ -30,25 +30,15 @@ def random_image() -> Response:
     return send_image(image)
 
 
-@app.route("/debug/list")
-def list_images() -> Response:
-    images = list(CONFIG.paths.image_dir.glob("*.jpg"))
-    return {
-        "count": len(images),
-        "images": [p.name for p in images],
-    }
-
-
-@app.route("/debug/random")
-def random_image_without_cache() -> Response:
-    invalidate_cache()
-    image = get_random_image()
-    return send_from_directory(CONFIG.paths.image_dir, image.name)
-
-
 @app.route("/")
+@app.route("/daily_view", strict_slashes=False)
 def daily_view():
-    return render_template("daily.html")
+    return render_template("daily.html", img_src="/daily")
+
+
+@app.route("/random_view", strict_slashes=False)
+def random_view():
+    return render_template("daily.html", img_src="/random")
 
 
 if __name__ == "__main__":
